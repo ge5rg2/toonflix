555b802e3ff1ad35a053955ee35f1778d0412e76
Kyuhyeon <kyuhyeon.kim@doosan.com>
2024-03-04 17:36:10 +0900

    feat: 4.0 user interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
 // st -> shortcut to make a StatefulWidget
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool hideTitle = true;
-
-  void onToggle() {
-    setState(() {
-      hideTitle = !hideTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.amber,
-            fontSize: 40,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: onToggle,
-                icon: const Icon(Icons.highlight_remove_rounded),
-              ),
-              hideTitle ? const Text("nothing") : const MyLargeTitle(),
-            ],
-          ),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print('init');
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    print('dispose');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "My Large Title",
-      style: TextStyle(
-        fontSize: Theme.of(context).textTheme.titleLarge?.fontSize,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
-    );
-  }
-}
-
-/**
- * [BottomAppBar] and a [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order to
- */
