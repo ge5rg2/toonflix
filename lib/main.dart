b14484febd8c6d5ba064b1b987e52a79779fa5fc
Kyuhyeon <kyuhyeon.kim@doosan.com>
2024-03-04 11:56:23 +0900

    feat: make a currency card

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:toonflix/widgets/button.dart';
 import 'package:toonflix/widgets/currency_card.dart';
+import 'package:toonflix/dummy/card_data.dart';
 
 void main() {
   runApp(const App());
@@ -16,132 +17,123 @@ class App extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       backgroundColor: const Color(0xFF181818),
-      body: SingleChildScrollView(
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          "Hey, George",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 34,
-                              fontWeight: FontWeight.w800),
-                        ),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(
-                            fontSize: 22,
-                            color: Colors.white.withOpacity(0.8),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 70,
-                ),
-                Text(
-                  "Total Blance",
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.8),
-                    fontSize: 22,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text(
-                  "\$5 194 382",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    color: Colors.white,
-                    fontSize: 48,
-                  ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                      text: 'Transfer',
-                      bgColor: Colors.amber,
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: "Request",
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     const Text(
-                      "Wallets",
+                      "Hey, George",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.w600,
-                      ),
+                          color: Colors.white,
+                          fontSize: 34,
+                          fontWeight: FontWeight.w800),
                     ),
                     Text(
-                      "View All",
+                      "Welcome back",
                       style: TextStyle(
+                        fontSize: 22,
                         color: Colors.white.withOpacity(0.8),
-                        fontSize: 18,
                       ),
                     ),
                   ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 70,
+            ),
+            Text(
+              "Total Blance",
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.8),
+                fontSize: 22,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              "\$5 194 382",
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+                fontSize: 48,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Button(
+                  text: 'Transfer',
+                  bgColor: Colors.amber,
+                  textColor: Colors.black,
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CurrencyCard(
-                  name: "Euro",
-                  code: "EUR",
-                  amount: "6,423",
-                  icon: Icons.euro_symbol_rounded,
-                ),
-                Transform.translate(
-                  offset: const Offset(0, -20),
-                  child: const CurrencyCard(
-                    name: "Bitcoin",
-                    code: "BTC",
-                    amount: "9,45",
-                    icon: Icons.currency_bitcoin_rounded,
-                    textColor: Colors.black,
-                    bgColor: Colors.white,
+                Button(
+                  text: "Request",
+                  bgColor: Color(0xFF1F2123),
+                  textColor: Colors.white,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Wallets",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                Transform.translate(
-                  offset: const Offset(0, -40),
-                  child: const CurrencyCard(
-                    name: "Dollar",
-                    code: "USD",
-                    amount: "147,998",
-                    icon: Icons.attach_money_rounded,
+                Text(
+                  "View All",
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 18,
                   ),
                 ),
               ],
-            )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: cardsData.length,
+                itemBuilder: (context, index) {
+                  final card = cardsData[index];
+
+                  return CurrencyCard(
+                    name: card["name"],
+                    code: card["code"],
+                    amount: card["amount"],
+                    icon: card["icon"],
+                    textColor: card["textColor"],
+                    bgColor: card["bgColor"],
+                    transfer: card["transfer"],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     ));
   }
