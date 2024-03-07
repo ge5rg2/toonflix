b01819ad4c6e809482eb32f0c82523776d94f0ea
Kyuhyeon <kyuhyeon.kim@doosan.com>
2024-03-07 15:14:14 +0900

    feat: 6.13 detail info

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/%08webtoon_detail.dart';
+import 'package:toonflix/models/webtoom_episode.dart';
+import 'package:toonflix/services/api_service.dart';
 import 'package:toonflix/widgets/header_widget.dart';
 import 'package:toonflix/widgets/imageCard_widget.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
   const DetailScreen({
     super.key,
@@ -12,11 +15,26 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(50),
-        child: HeaderContainer(header: title),
+        child: HeaderContainer(header: widget.title),
       ),
       body: Column(
         children: [
@@ -27,11 +45,51 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ImageContainer(
-                thumb: thumb,
-                id: id,
+                thumb: widget.thumb,
+                id: widget.id,
               ),
             ],
-          )
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: TextStyle(
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .fontSize),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: TextStyle(
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .fontSize),
+                      )
+                    ],
+                  ),
+                );
+              }
+              return const Text("...");
+            },
+          ),
         ],
       ),
     );
